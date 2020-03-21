7e1b1caf4c91d684735fe4cf3d55c3e6871f7c3b
ryutahayashi <ryu.hayac@gmail.com>
2020-03-22 00:55:14 +0900

    edited comment 3

--- yahoonews.py
+++ yahoonews.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 # yahoo news headline
 url = "https://news.yahoo.co.jp/"
+# changed it to urllib.request from Requests
 uh = urllib.request.urlopen(url)
 data = uh.read().decode()
 soup = BeautifulSoup(data, "html.parser")
